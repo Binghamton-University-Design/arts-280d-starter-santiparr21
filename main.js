22aba6299b95933d38106e88277bcc4d516303c7
santiparr21 <sparra2@binghamton.edu>
2021-07-06 18:39:48 -0400

    home page
    part 1

---
+++ main.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function() {
+
+var $hamburger = $(".hamburger");
+  $hamburger.on("click", function(e) {
+    $hamburger.toggleClass("is-active");
+    $('#nav').toggleClass('active');
+  });
+
+});
